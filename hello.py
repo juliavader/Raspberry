7f18336eae7d8c4c330ae29bacc6ad12d0bfcf74
rjubault <romainjubault@gmail.com>
2019-01-31 15:37:42 +0000

    ajout de la classe led

--- hello.py
+++ hello.py
@@ -1,41 +1,46 @@
 from flask import Flask
 app = Flask(__name__)
-import RPi.GPIO as GPIO
 import time
+from flask import render_template 
+from led import Led
 
+led1 = Led(14)
+led2 = Led(12)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!<br><a href="/on">Allumer la led</a>'
+    return render_template('base.html')
 @app.route('/hello/<name>')
 def hello_name(name):
     return 'Hello, %s !' % name
-@app.route('/on')
-def led_on():
-    #Utilisation d'une norme de nommage pour les broches
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
 
-    #initialisation de la broche en mode "sortie"
-    # Le nombre passé en paramètre correspond au numéro de GPIO et non au numéro de l
-    GPIO.setup(14, GPIO.OUT)
-    GPIO.setup(12, GPIO.OUT)
-    #On indique à la pin GPIO 14 que l'on veut envoyer du courant sur celle-ci
-    GPIO.output(14, GPIO.HIGH)
-    GPIO.output(12, GPIO.HIGH)
-    return '<a href="/off">Éteindre les leds </a>'
-    GPIO.cleanup()
-@app.route('/off')
-def led_off():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-    GPIO.setup(14, GPIO.OUT)
-    GPIO.setup(12, GPIO.OUT)
+@app.route('/led/')
+@app.route('/led/<state>')
+@app.route('/led/<state>/<name>')
+def led(state=None, name=None):
+    led = None
     
-    GPIO.output(14, GPIO.LOW)
-    GPIO.output(12, GPIO.LOW)
-    return '<a href="/on"> allumer les leds </a>'
-    GPIO.cleanup()
+    if(name == 'led1'):
+        led=led1
+    elif(name == 'led2'):
+        led=led2
+    else:
+        name=None
+
+    if(state == 'on'):
+        if(name):
+            led.allumer()
+        else:
+            led1.allumer()
+            led2.allumer()
+    elif(state == 'off'):
+        if(name):
+            led.eteindre()
+        else:
+            led1.eteindre()
+            led2.eteindre()
+    return render_template('hello.html',on=state,led1=led1.statut, led2=led2.statut)
+
 @app.route('/on/<name>')
 def spec_on(name):
     #Utilisation d'une norme de nommage pour les broches
