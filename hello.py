317e7505f70edb0de33e4b48cc5436f405f06144
rjubault <romainjubault@gmail.com>
2019-01-31 09:27:50 +0000

    first raspberry project

---
+++ hello.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+@app.route('/hello/<name>')
+def hello_name(name):
+    return 'Hello, %s !' % name
